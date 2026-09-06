c6e5a91a319dd2f359860896331342f4df001a49
James Doggett <jdoggett1@buffs.wtamu.edu>
2026-09-06 14:19:31 -0500

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+    courses = [
+        {"name": "Web Development", "completed": False},
+        {"name": "Programming Fundamentals", "completed": True},
+        {"name": "Networking", "completed": True},
+        {"name": "Cybersecurity", "completed": False},
+        {"name": "Database Management", "completed": True},
+    ]
+
+    return render(request, "home.html", {"items": courses})
 
 
 def about(request):
