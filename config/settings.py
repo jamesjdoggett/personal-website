0ef652c18b25ec842b276811eba49f6a8db32c43
James Doggett <jdoggett1@buffs.wtamu.edu>
2026-09-06 13:48:14 -0500

    Register the pages app and configure URLs

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,4 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
